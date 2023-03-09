0e8cd90f205381dcec7c5c5f6f87768cd9f93e14
EbrarMutlu <71319071+EbrarMutlu@users.noreply.github.com>
2023-03-09 14:21:32 +0300

    Update generate_maze.py
    printing out the maze with random I, G and * objects randomly chosen each time
    0's just represent the paths they don't mean anything else

--- generate_maze.py
+++ generate_maze.py
@@ -1,89 +1,38 @@
+# Define the maze as a 2D list with 60 rows and 80 columns
 import random
 
-# Define the size of the maze
-ROWS = 60
-COLS = 80
+maze = [[0 for j in range(80)] for i in range(60)]
+# Define the obstacle object
+obstacle = '*'
+initial = 'I'
+goal = 'G'
 
-# Define the fraction of maze elements that will contain an obstacle
-OBSTACLE_DENSITY = 0.3
+initial_placerow = random.randint(0,59)
+initial_placecolumn = random.randint(0,79)
 
-# Define the possible states of the agent
-EMPTY = ' '
-OBSTACLE = '*'
-INITIAL_STATE = 'I'
-GOAL_STATE = 'G'
-OPTIMAL_PATH = '+'
+goal_placerow = random.randint(0,59)
+goal_placecolumn = random.randint(0,79)
 
-# Define the A* algorithm with a suitable heuristic function
-def astar(maze, start, end):
-    def heuristic(node):
-        return abs(node[0]-end[0]) + abs(node[1]-end[1])
-    
-    # Initialize the open and closed sets
-    open_set = {start}
-    closed_set = set()
-    
-    # Initialize the dictionary to store the optimal path
-    came_from = {}
-    
-    # Initialize the dictionary to store the cost of reaching each node
-    g_score = {start: 0}
-    
-    # Initialize the dictionary to store the estimated cost of reaching the goal from each node
-    f_score = {start: heuristic(start)}
-    
-    while open_set:
-        # Find the node with the lowest estimated cost to the goal
-        current = min(open_set, key=lambda x: f_score[x])
-        
-        # If we have reached the goal, return the optimal path
-        if current == end:
-            path = []
-            while current in came_from:
-                path.append(current)
-                current = came_from[current]
-            path.reverse()
-            return path
-        
-        # Move the current node from the open set to the closed set
-        open_set.remove(current)
-        closed_set.add(current)
-        
-        # Explore the neighbors of the current node
-        for neighbor in [(current[0]-1, current[1]), (current[0]+1, current[1]), (current[0], current[1]-1), (current[0], current[1]+1)]:
-            # Check if the neighbor is valid and not already explored
-            if 0 <= neighbor[0] < ROWS and 0 <= neighbor[1] < COLS and maze[neighbor[0]][neighbor[1]] != OBSTACLE and neighbor not in closed_set:
-                # Calculate the cost of reaching the neighbor from the current node
-                tentative_g_score = g_score[current] + 1
-                
-                # Add the neighbor to the open set if it is not already there
-                if neighbor not in open_set:
-                    open_set.add(neighbor)
-                elif tentative_g_score >= g_score[neighbor]:
-                    continue
-                
-                # Record the optimal path to the neighbor
-                came_from[neighbor] = current
-                
-                # Record the cost of reaching the neighbor
-                g_score[neighbor] = tentative_g_score
-                
-                # Record the estimated cost of reaching the goal from the neighbor
-                f_score[neighbor] = tentative_g_score + heuristic(neighbor)
-    
-    # If we reach this point, there is no path from the start to the goal
-    return None
 
-# Create the maze
-maze = [[EMPTY for j in range(COLS)] for i in range(ROWS)]
 
-# Add obstacles to the maze
-num_obstacles = int(ROWS * COLS * OBSTACLE_DENSITY)
-for i in range(num_obstacles):
-    row = random.randint(0, ROWS-1)
-    col = random.randint(0, COLS-1)
-    maze[row][col] = OBSTACLE
+# Fill the maze with 30% obstacles chosen randomly
+for i in range(60):
+    for j in range(80):
+        if random.random() < 0.3:  # 30% chance of adding an obstacle
+            maze[i][j] = obstacle
 
-# Select the initial and goal states
-while True:
-    start = (random.randint(0, ROWS-1
+while maze[initial_placerow][initial_placecolumn] == obstacle:
+    initial_placerow = random.randint(0, 59)
+    initial_placecolumn = random.randint(0, 79)
+maze[initial_placerow][initial_placecolumn] = initial
+
+while maze[goal_placerow][goal_placecolumn] == obstacle:
+    goal_placerow = random.randint(0, 59)
+    goal_placecolumn = random.randint(0, 79)
+maze[goal_placerow][goal_placecolumn] = goal
+
+# Print the maze
+for i in range(60):
+    for j in range(80):
+        print(maze[i][j], end=' ')
+    print()
