20c7771dd1915bae6f7e081208bae03371ff3fa7
EbrarMutlu <71319071+EbrarMutlu@users.noreply.github.com>
2023-03-16 14:59:00 +0300

    Update generate_maze.py
    I changed 0's to empty spaces. Also if the initial or goal chosen happens to be in the place of an obstacle, the program quits printing an error message

--- generate_maze.py
+++ generate_maze.py
@@ -1,7 +1,10 @@
 # Define the maze as a 2D list with 60 rows and 80 columns
 import random
+import sys
 
-maze = [[0 for j in range(80)] for i in range(60)]
+
+maze = [['' for j in range(80)] for i in range(60)]
+#we can change blank space to 0 or vice versa
 # Define the obstacle object
 obstacle = '*'
 initial = 'I'
@@ -22,17 +25,22 @@ for i in range(60):
             maze[i][j] = obstacle
 
 while maze[initial_placerow][initial_placecolumn] == obstacle:
-    initial_placerow = random.randint(0, 59)
-    initial_placecolumn = random.randint(0, 79)
-maze[initial_placerow][initial_placecolumn] = initial
+    print("The initial state is occupied by an obstacle")
+    sys.exit()
+    #initial_placerow = random.randint(0, 59)
+    #initial_placecolumn = random.randint(0, 79)
+#maze[initial_placerow][initial_placecolumn] = initial
 
 while maze[goal_placerow][goal_placecolumn] == obstacle:
-    goal_placerow = random.randint(0, 59)
-    goal_placecolumn = random.randint(0, 79)
-maze[goal_placerow][goal_placecolumn] = goal
+    print("The  goal state is occupied by an obstacle")
+    sys.exit()
+    #goal_placerow = random.randint(0, 59)
+    #goal_placecolumn = random.randint(0, 79)
+#maze[goal_placerow][goal_placecolumn] = goal
 
 # Print the maze
 for i in range(60):
     for j in range(80):
         print(maze[i][j], end=' ')
+
     print()
