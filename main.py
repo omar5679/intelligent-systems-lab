cc6706f9953c46d7a15a2b952b9be1a7fc8c14c1
Michal <michjak50@gmail.com>
2023-03-29 01:39:06 +0200

    Exercise done

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from algorithm import AStar
-from generate_maze import generate_maze, print_maze, update_maze
+from generate_maze import generate_maze, update_maze, print_maze_to_file
 
 def main():
-    
+
     a_star = AStar()
     maze, init_state, goal_state = generate_maze()
     path = a_star.algorithm(maze, init_state, goal_state)
 
-    print_maze(maze)
-
     updated_maze = update_maze(maze, path)
-    print('\n\n\n')
-    print_maze(updated_maze)
-   
+    print_maze_to_file(updated_maze, 'maze.txt')
+       
 
 if __name__ == '__main__':
     main()
